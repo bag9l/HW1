e884b0bd75a7b126f5a82534997e7fe02fb63c04
bag9l <76074096+bag9l@users.noreply.github.com>
2021-03-21 12:54:48 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,89 +1,95 @@
 package com.company;
+
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-    /////////////////////////////////////////////////////////
-
-        // Сортування масиву:
+    public static void arraySort(int[] A) {
         System.out.println("1. Sort the array:");
-        int ArrForSort[]={2,3,1,7,11};
         int temp;
-        System.out.printf("\tInitial array: ");
-        for (int i = 0; i < ArrForSort.length; i++)
-            System.out.printf(ArrForSort[i]+" ");
+        System.out.print("\tInitial array: ");
+        for (int i = 0; i < A.length; i++)
+            System.out.print(A[i] + " ");
         System.out.println();
-        for (int i = 0; i < ArrForSort.length; i++) {
-            for (int j = 0; j < ArrForSort.length; j++) {
-                if(ArrForSort[i]>ArrForSort[j] && i!=j) {
-                    temp = ArrForSort[j];
-                    ArrForSort[j] = ArrForSort[i];
-                    ArrForSort[i] = temp;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A.length; j++) {
+                if (A[i] > A[j] && i != j) {
+                    temp = A[j];
+                    A[j] = A[i];
+                    A[i] = temp;
                 }
             }
         }
-        System.out.printf("\tSorted array: ");
-        for (int i = 0; i < ArrForSort.length; i++)
-            System.out.printf(ArrForSort[i]+" ");
+        System.out.print("\tSorted array: ");
+        for (int i = 0; i < A.length; i++)
+            System.out.print(A[i] + " ");
         System.out.println("\n");
+    }
 
-    /////////////////////////////////////////////////////////
-
-        // Сума додатніх елементів масиву:
+    public static void arraySum() {
         System.out.println("2. The sum of positive elements of the array:");
         Scanner in = new Scanner(System.in);
-        System.out.printf("Enter the dimension of the array: ");
+        System.out.print("Enter the dimension of the array: ");
         int size = in.nextInt();
-        int ArrForSum[]=new int[size];
+        int[] arrForSum = new int[size];
         System.out.println("Enter array elements:");
         for (int i = 0; i < size; i++) {
-            System.out.print("Array["+i+"] = ");
-            ArrForSum[i]=in.nextInt();
+            System.out.print("Array[" + i + "] = ");
+            arrForSum[i] = in.nextInt();
         }
-        int sum=0;
-        for (int i = 0; i < ArrForSum.length; i++) {
-            if(ArrForSum[i]>0)
-                sum+=ArrForSum[i];
+        int sum = 0;
+        for (int i = 0; i < arrForSum.length; i++) {
+            if (arrForSum[i] > 0)
+                sum += arrForSum[i];
         }
-        System.out.println("Sum: "+sum);
+        System.out.println("Sum: " + sum);
         System.out.println();
+        in.close();
+    }
 
-    /////////////////////////////////////////////////////////
-
-        // Середнє значення елементів:
+    public static void arrAverageElement(int[] A) {
         System.out.println("3. The middle element:");
-       int ArrForAverage[]={2,5,3,-2,66,0,-23,5,43};
-       int sumOfElements=0;
-       float average;
-        for (int i = 0; i < ArrForAverage.length; i++) {
-            sumOfElements+=ArrForAverage[i];
+
+        int sumOfElements = 0;
+        float average;
+        for (int i = 0; i < A.length; i++) {
+            sumOfElements += A[i];
         }
-        average=sumOfElements/ ArrForAverage.length;
-        for (int i = 0; i < ArrForAverage.length; i++)
-            System.out.printf(ArrForAverage[i]+" ");
+        average = sumOfElements / A.length;
+        for (int i = 0; i < A.length; i++)
+            System.out.print(A[i] + " ");
         System.out.println();
-        System.out.println("Average value of elements: "+ average);
+        System.out.println("Average value of elements: " + average);
         System.out.println();
+    }
 
-    /////////////////////////////////////////////////////////
-
-        // Заміна дубльованого елемента на нуль:
+    public static void arrDuplicate(int[] A) {
         System.out.println("4. Replacing a duplicate item with zero:");
-        int ArrWithoutDuplElements[]={3,2,3,1,4,2,8,3};
-        for (int i = 0; i < ArrWithoutDuplElements.length; i++)
-            System.out.printf(ArrWithoutDuplElements[i]+" ");
+        for (int i = 0; i < A.length; i++)
+            System.out.print(A[i] + " ");
 
-        for (int i = 0; i < ArrWithoutDuplElements.length; i++) {
-            for (int j = 0; j < ArrWithoutDuplElements.length; j++) {
-                if(ArrWithoutDuplElements[i]==ArrWithoutDuplElements[j] && i!=j)
-                    ArrWithoutDuplElements[j]=0;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A.length; j++) {
+                if (A[i] == A[j] && i != j)
+                    A[j] = 0;
             }
         }
         System.out.println();
-        for (int i = 0; i < ArrWithoutDuplElements.length; i++)
-            System.out.printf(ArrWithoutDuplElements[i]+" ");
+        for (int i = 0; i < A.length; i++)
+            System.out.print(A[i] + " ");
         System.out.println("\n");
     }
+
+    public static void main(String[] args) {
+        int[] arrForSort = {2, 3, 1, 7, 11};
+        int[] arrForAverage = {2, 5, 3, -2, 66, 0, -23, 5, 43};
+        int[] arrWithoutDuplElements = {2, -1, 45, 2, 12, 12, 11, -1};
+
+        arraySort(arrForSort);
+        arraySum();
+        arrAverageElement(arrForAverage);
+        arrDuplicate(arrWithoutDuplElements);
+
+    }
 }
+
