18f8d9d8ce4d090fc89b809b57bd784dfc4dc27c
bag9l <76074096+bag9l@users.noreply.github.com>
2021-03-21 13:55:34 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,8 +8,8 @@ public class Main {
         System.out.println("1. Sort the array:");
         int temp;
         System.out.print("\tInitial array: ");
-        for (int i = 0; i < arrForSort.length; i++) {
-            System.out.print(arrForSort[i] + " ");
+        for (int k : arrForSort) {
+            System.out.print(k + " ");
         }
         System.out.println();
         for (int i = 0; i < arrForSort.length; i++) {
@@ -22,8 +22,8 @@ public class Main {
             }
         }
         System.out.print("\tSorted array: ");
-        for (int i = 0; i < arrForSort.length; i++) {
-            System.out.print(arrForSort[i] + " ");
+        for (int j : arrForSort) {
+            System.out.print(j + " ");
         }
         System.out.println("\n");
     }
@@ -40,9 +40,9 @@ public class Main {
             arrForSum[i] = in.nextInt();
         }
         int sum = 0;
-        for (int i = 0; i < arrForSum.length; i++) {
-            if (arrForSum[i] > 0)
-                sum += arrForSum[i];
+        for (int j : arrForSum) {
+            if (j > 0)
+                sum += j;
         }
         System.out.println("Sum: " + sum);
         System.out.println();
@@ -53,13 +53,12 @@ public class Main {
         System.out.println("3. The middle element:");
 
         int sumOfElements = 0;
-        float average;
-        for (int i = 0; i < arrForAverage.length; i++) {
-            sumOfElements += arrForAverage[i];
+        for (int j : arrForAverage) {
+            sumOfElements += j;
         }
-        average = sumOfElements / arrForAverage.length;
-        for (int i = 0; i < arrForAverage.length; i++) {
-            System.out.print(arrForAverage[i] + " ");
+        float average = sumOfElements / arrForAverage.length;
+        for (int j : arrForAverage) {
+            System.out.print(j + " ");
         }
         System.out.println();
         System.out.println("Average value of elements: " + average);
@@ -68,8 +67,8 @@ public class Main {
 
     public static void getArrWithoutDuplElements(int[] arrWithDuplElements) {
         System.out.println("4. Replacing a duplicate item with zero:");
-        for (int i = 0; i < arrWithDuplElements.length; i++) {
-            System.out.print(arrWithDuplElements[i] + " ");
+        for (int arrWithDuplElement : arrWithDuplElements) {
+            System.out.print(arrWithDuplElement + " ");
         }
         for (int i = 0; i < arrWithDuplElements.length; i++) {
             for (int j = 0; j < arrWithDuplElements.length; j++) {
@@ -78,8 +77,8 @@ public class Main {
             }
         }
         System.out.println();
-        for (int i = 0; i < arrWithDuplElements.length; i++) {
-            System.out.print(arrWithDuplElements[i] + " ");
+        for (int arrWithDuplElement : arrWithDuplElements) {
+            System.out.print(arrWithDuplElement + " ");
         }
         System.out.println("\n");
     }
